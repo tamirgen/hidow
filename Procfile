e5f55f0a8378961b2bb2a060ba9ba77787ad8002
tamirgen <tamirgenkin@gmail.com>
2022-06-01 13:31:23 +0000

    Deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hidow.wsgi:application
--- hidow/settings.py
+++ hidow/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-y-h8-!l!%$u@&25r_@4z38ku!ldv%0$^nh%eec5ihwovxs+a!#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hidow.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,17 @@ WSGI_APPLICATION = 'hidow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
