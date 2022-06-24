fff6ff52584cca2678cc238531a85be71b5b14a6
tamirgen <tamirgenkin@gmail.com>
2022-06-24 05:18:28 +0000

    Fixed footer formating issue

--- hidow/settings.py
+++ hidow/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'a')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 
 
